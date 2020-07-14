18381e4745288ead3e2a85c019ef335576ea900a
AzureAD\AbhishekKumar <abhishek@monocept.com>
2020-07-14 13:14:49 +0530

    Updates

---
+++ precache-manifest.9227274f2cc5b29e72476df91fd3f5b2.js
@@ -0,0 +1,44 @@
+self.__precacheManifest = [
+  {
+    "revision": "330894d1d35ac9eca6be",
+    "url": "/bundle.js"
+  },
+  {
+    "revision": "d2d5e62872123325a10f",
+    "url": "/1.bundle.js"
+  },
+  {
+    "url": "/db54f7ae404ddb39a1f81a42e70f4b63.ico"
+  },
+  {
+    "url": "/b8c36ae16f52d532b9c2581769a68415.png"
+  },
+  {
+    "url": "/10acbfd757ed3d1a7eb7f86999987be6.png"
+  },
+  {
+    "url": "/26b0a993c6e53cb368065e1246915693.png"
+  },
+  {
+    "revision": "502e410cef7867083d9adde63468b45c",
+    "url": "/index.html"
+  },
+  {
+    "url": "/icon_512x512.a3e0816193e58cf121387610d50752a8.png"
+  },
+  {
+    "url": "/icon_384x384.35f38d3583f54c90fa9f4d057c4e567e.png"
+  },
+  {
+    "url": "/icon_256x256.328ce918ade7210044e44edefbccfa22.png"
+  },
+  {
+    "url": "/icon_192x192.b44dc884a7956a7ae3c7a6f2365bbe1b.png"
+  },
+  {
+    "url": "/icon_128x128.a53fa7f8d7c1d8923c24f90da86c8ca0.png"
+  },
+  {
+    "url": "/icon_96x96.5d70e41fed86a50bedd2a40b38b00de2.png"
+  }
+];
